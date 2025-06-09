e449a38fa793ba40fc79b2735d6ecdc0bda6f06e
chintan <chintanpatel1081@gmail.com>
2025-06-09 16:23:44 +0530

    frontend

--- components/ChatsNavbar.tsx
+++ components/ChatsNavbar.tsx
@@ -8,7 +8,7 @@ const ChatsNavbar = () => {
                             <div className="w-10 h-10 bg-gradient-to-r from-purple-600 to-indigo-600 rounded-md flex items-center justify-center">
                                 <div className="text-white font-bold text-lg">2D</div>
                             </div>
-                            AnimaGraph
+                            AnimaGraph      
                         </h1>
                         <div className="text-white text-lg flex items-center justify-end">
                             <div className="p-2 hover:bg-[#656565] rounded cursor-pointer"><FaLinkedin /></div>
